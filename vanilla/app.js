b94dc0147dd58c99826250200ff63a76f78473ec
hongminhwa <minhwa101@gmail.com>
2024-02-26 18:03:39 +0900

    my Second Commit

--- vanilla/app.js
+++ vanilla/app.js
@@ -1,9 +1,45 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
 
 
-function handleClick() {  
-   h1.classList.toggle("click"); 
+const loginInput =  document.querySelector("#login-form input"); 
+const greeting = document.querySelector("#greeting");
+
+const HidingClass = "hidden";
+
+const USERNAME_KEY = "username";
+
+
+function handleLogin(event) {
+   event.preventDefault(); 
+   loginForm.classList.add(HidingClass); 
+   const username = loginInput.value; 
+   localStorage.setItem(USERNAME_KEY, username); 
+   console.log(username); 
+   paintGreetings(username);
+
+}; 
+
+
+
+function paintGreetings(username) {
+   greeting.innerText = `hello ${username}`
+   greeting.classList.remove(HidingClass); 
+   
+};
+
+
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY); 
+
+if(savedUsername === null) { 
+   loginForm.classList.remove(HidingClass);
+   loginForm.addEventListener("submit", handleLogin);
+   //show form 
+}else {
+   //delete form
+  paintGreetings(savedUsername);  
 }
 
 
-h1.addEventListener("click", handleClick);  
+
